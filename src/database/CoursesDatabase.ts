05014114c6158b447ee3c1e2414f1165039ee117
WeslleiBrito <wesllei.wbs@gmail.com>
2023-08-03 13:21:52 -0300

    Criado dois erros e a validação das strings

--- src/database/CoursesDatabase.ts
+++ src/database/CoursesDatabase.ts
@@ -1,6 +1,19 @@
 import { DatabaseConnect } from "./DatabaseConnect";
 
 
-export abstract class CourseDatabase extends DatabaseConnect {
+export class CourseDatabase extends DatabaseConnect {
 
+    public NAME_TABLE_COURSES: string = "courses"
+
+    public createCourse = async (input: any): Promise<number>  => {
+        const {
+            id,
+            name, 
+            lessons
+        } = input
+
+        const [create] = await DatabaseConnect.connection(this.NAME_TABLE_COURSES).insert({id, name, lessons})
+
+        return create
+    }
 }
